cefac76dcd57b8ae65920fa916fdaab6d36df816
STTSAI <shinting.tsai@gmail.com>
2020-03-20 21:28:19 +0800

    add Range for advanced task requirement

--- index.js
+++ index.js
@@ -4,19 +4,50 @@
   let blue = document.getElementById("blue")
   let hex = document.getElementById("hex")
   let convert = document.getElementById("conBtn")
+  let redRange = document.getElementById("redRange")
+  let greenRange = document.getElementById("greenRange")
+  let blueRange = document.getElementById("blueRange")
  
   convert.addEventListener("submit", event => {
+    event.preventDefault();
     let result = checkInt(red.value) && checkInt(green.value) && checkInt(blue.value)
     if (result) {
-      document.getElementById("redSample").style.backgroundColor = `rgb(${red.value}, 0, 0)`;
-      document.getElementById("greenSample").style.backgroundColor = `rgb(0, ${green.value}, 0)`;
-      document.getElementById("blueSample").style.backgroundColor = `rgb(0, 0, ${blue.value})`;
-      hex.value = '#' + toHex(red.value) + toHex(green.value) + toHex(blue.value)
-      document.getElementById("hexSample").style.backgroundColor = hex.value
+      // document.getElementById("redSample").style.backgroundColor = `rgb(${red.value}, 0, 0)`;
+      // document.getElementById("greenSample").style.backgroundColor = `rgb(0, ${green.value}, 0)`;
+      // document.getElementById("blueSample").style.backgroundColor = `rgb(0, 0, ${blue.value})`;
+      redRange.value = red.value
+      greenRange.value = green.value
+      blueRange.value = blue.value
+      updateHex(red.value, green.value, blue.value)
+
+
     } else {
       alert('Please type in the integer between 0 to 255')
     }
   })
+
+  $(document).on('input', '#redRange', function () {
+    red.value = event.target.value
+    updateHex(red.value, green.value, blue.value)
+  });
+
+  $(document).on('input', '#greenRange', function () {
+    green.value = event.target.value
+    updateHex(red.value, green.value, blue.value)
+  });
+
+  $(document).on('input', '#blueRange', function () {
+    blue.value = event.target.value
+    updateHex(red.value, green.value, blue.value)
+  });
+
+  function updateHex(r, g, b) {
+    document.getElementById("redSample").style.backgroundColor = `rgb(${r}, 0, 0)`;
+    document.getElementById("greenSample").style.backgroundColor = `rgb(0, ${g}, 0)`;
+    document.getElementById("blueSample").style.backgroundColor = `rgb(0, 0, ${b})`;
+    hex.value = '#' + toHex(r) + toHex(g) + toHex(b)
+    document.getElementById("hexSample").style.backgroundColor = hex.value
+  }
 
   function toHex(input) {
     let result = Number(input).toString(16)
@@ -33,4 +64,23 @@
     } else return false;
   }
 
+  function change(value) {
+    red.value = value
+    updateHex(red.value, green.value, blue.value)
+
+  }
 })()
+
+// show value after mouse-up, not real time
+  // redRange.addEventListener("change", event => {
+  //   red.value = event.target.value
+  //   updateHex(red.value, green.value, blue.value)
+  // })
+  // greenRange.addEventListener("change", event => {
+  //   green.value = event.target.value
+  //   updateHex(red.value, green.value, blue.value)
+  // })
+  // blueRange.addEventListener("change", event => {
+  //   blue.value = event.target.value
+  //   updateHex(red.value, green.value, blue.value)
+  // })
